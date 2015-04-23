0bb3c619120d5fd283c0f7115ff8ef5cbf9dcb92
Sergey Ivanov <xifrin@gmail.com>
2015-04-23 16:53:31 +0600

    __main__

---
+++ github_art/__main__.py
@@ -0,0 +1,38 @@
+import optparse
+
+from github import Github
+
+
+parser = optparse.OptionParser(conflict_handler="resolve")
+parser.add_option('--string', action='store', type='string', \
+                  dest='string', default=None, \
+                  help='Art string for Github Contributions table'
+)
+parser.add_option('--size', action='store', type='string', \
+                  dest='size', default=2, \
+                  help='Size of string\'s letters'
+)
+parser.add_option('--project', action='store', type='string', \
+                  dest='project', help='Github git URL'
+)
+parser.add_option('--path', action='store', type='string', \
+                  dest='path', default='../build', \
+                  help='Path to github new project'
+)
+
+options, arguments = parser.parse_args()
+
+if not options.string:
+    parser.error('String not given')
+
+if not options.path:
+    parser.error('Build git path not given')
+
+if not options.account:
+    parser.error('Github git URL not given')
+
+git = Github(options.string, options.path, options.size)
+git.initialite()
+if len(options.project) > 0:
+    git.set_account(options.project)
+git.run()
