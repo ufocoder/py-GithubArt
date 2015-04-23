1d5b4f3bdfbca86a48ec24309a9604c3d031b3cc
Sergey Ivanov <xifrin@gmail.com>
2015-04-23 15:38:45 +0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='github_art',
+      version='0.2',
+      packages=['github_art'],
+      description='Github Contributions table Writer. Just for fun',
+      author='Sergey Ivanov',
+      author_email='xufocoder@gmail.com',
+      license='MIT',
+      install_requires = ['python-dateutil>=2.4.2'],
+      url='https://github.com/ufocoder/py-GithubArt'
+)
